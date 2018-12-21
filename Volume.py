8ed33083dffa2fb4d3391af6514af3ab785167aa
AntonioLegnante <AntonioLegnante12@gmail.com>
2018-12-21 22:15:30 +0100

    Calcolo del volume ha scelta dell'utente tra Sfera e Cubo

--- Volume.py
+++ Volume.py
@@ -3,9 +3,9 @@ import math
 Scelta=int (input ("1 - Cubo, 2 - Sfera "))
 if Scelta==1 :
 		Lato=int (input ("Inserisci il lato "))
-		print ("Il volume è ", Lato**3)
+		print ("Il volume e' ", Lato**3)
 elif Scelta==2 :
 		Raggio=int (input ("Inserisci il Raggio "))
-		print ("Il volume è ", 4/3*math.pi*Raggio**3)
+		print ("Il volume e' ", 4/3*math.pi*Raggio**3)
 else :
 	print("Sai leggere?")
