f0199c626a07912801257c8ffb4d36a23f92a19b
AntonioLegnante <AntonioLegnante12@gmail.com>
2018-12-21 20:38:05 +0100

    Individuare se un numero dato in input è pari o dispari

--- paridisp.py
+++ paridisp.py
@@ -1,6 +1,6 @@
 #Individuare se un numero dato in input sia pari o dispari
 numero=int (input ("inserisci un numero "))
-if numero%2==0:
-		print ("Il numero è pari")
-else:
-	print ("Il numero è dispari")
+if numero%2==0 :
+			print ("Il numero è pari")
+else :
+			print ("Il numero è dispari")
