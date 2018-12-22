6e5486788c144e44f27b3375720b4b6de8d0a0d5
AntonioLegnante <AntonioLegnante12@gmail.com>
2018-12-22 10:54:38 +0100

    Calcolo della Superficie e del Volume di una Sfera

--- Sfera.py
+++ Sfera.py
@@ -1,5 +1,5 @@
 #Superficie e Volume di una Sfera
 import math 
 Raggio= int (input ("Inserisci il raggio "))
-print ("La superficie e' ", 4. *Raggio*math.pi**2) 
-print ("Il volume e' ", 4. / 3. *Raggio*math.pi**3)
+print ("La superficie e' ", 4. *math.pi*Raggio**2) 
+print ("Il volume e' ", 4. / 3. *math.pi*Raggio**3)
